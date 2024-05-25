f1b0126b091d642ff77cd10ef8de1cc72166a849
Manyeya <siemakhutso@gmail.com>
2024-05-25 20:23:49 +0200

    chore: Update layout and add lazy loading for images

---
+++ app/ui/fonts.ts
@@ -0,0 +1,8 @@
+import { Allura, Average,Montserrat } from 'next/font/google'
+
+export const allura = Montserrat({
+    subsets: ['latin'],
+    weight: '500'
+})
+
+export const average = Average({ subsets: ['latin'], weight: '400' })
