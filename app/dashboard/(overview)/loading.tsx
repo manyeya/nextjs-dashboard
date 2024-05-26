cc551ad4f5d68e99f7a3e4abcb927cebe85284ce
Manyeya <siemakhutso@gmail.com>
2024-05-26 13:43:16 +0200

    refactor: Remove unused font import and update font variable name

---
+++ app/dashboard/(overview)/loading.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import DashboardSkeleton from '@/app/ui/skeletons';
+type Props = {}
+
+const loading = (props: Props) => {
+    return (
+        <DashboardSkeleton />
+    )
+}
+
+export default loading
